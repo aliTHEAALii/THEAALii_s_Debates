55c2c8f60903596b267d35838960432cfc669345
aliTHEAALii <ali.THEAALii@gmail.ceom>
2024-08-08 13:39:40 -0700

    Added - Search Tab

---
+++ THEAALiisDebates/VIEW/TI/TI Control Center/UploadToTIChainFSC.swift
@@ -0,0 +1,18 @@
+//
+//  UploadToTIChainFSC.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 8/3/24.
+//
+
+import SwiftUI
+
+struct UploadToTIChainFSC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadToTIChainFSC()
+}
