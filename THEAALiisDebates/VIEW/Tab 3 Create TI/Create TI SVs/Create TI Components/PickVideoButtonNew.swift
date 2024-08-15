64648b17f7c2722fcbbdab54725ac90bb7b4d28c
aliTHEAALii <ali.THEAALii@gmail.ceom>
2024-08-14 22:21:24 -0700

    Video Changes - - VL Post Content Size (width) - Video Size & duration

---
+++ THEAALiisDebates/VIEW/Tab 3 Create TI/Create TI SVs/Create TI Components/PickVideoButtonNew.swift
@@ -0,0 +1,18 @@
+//
+//  PickVideoButtonNew.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 8/14/24.
+//
+
+import SwiftUI
+
+struct PickVideoButtonNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickVideoButtonNew()
+}
